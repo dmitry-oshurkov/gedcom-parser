394b70bf0947b5ecc0b0aeae279a24886e18fb02
dmitry-oshurkov <dmitry@oshurkov.name>
2017-04-18 17:16:34 +0300

    minor refactoring

--- src/Lib.hs
+++ src/Lib.hs
@@ -98,11 +98,8 @@ parseName obj (level, tag, value) nextTags (people, families) continue
 
 parseSourceCitation obj (level, tag, value) nextTags (people, families) continue
     | tag == "PAGE" = continue $ set page (read value :: Int) obj
-    | tag == "EVEN" = bodyOf (justNewEvent (parseEventType value) value) level (tail nextTags) (people, families) continue' parseEvent
+    | tag == "EVEN" = bodyOf (justNewEvent (parseEventType value) value) level (tail nextTags) (people, families) continue' parseEvent -- EVEN [  <EVENT_TYPE_INDIVIDUAL> | <EVENT_TYPE_FAMILY> | <ATTRIBUTE_TYPE> ]        -- ATTRIBUTE_TYPE: = {Size=1:4}               [ CAST | EDUC | NATI | OCCU | PROP | RELI | RESI | TITL ]
     | otherwise = continue obj
-        -- EVEN [  <EVENT_TYPE_INDIVIDUAL> | <EVENT_TYPE_FAMILY> | <ATTRIBUTE_TYPE> ]
-        -- ATTRIBUTE_TYPE: = {Size=1:4}               [ CAST | EDUC | NATI | OCCU | PROP | RELI | RESI | TITL ]
-
 -- n SOUR @<XREF:SOUR>@    /* pointer to source record */  {1:1}
 --       +2 ROLE <ROLE_IN_EVENT>  {0:1}
 --     +1 DATA        {0:1}
